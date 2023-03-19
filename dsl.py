405149e0ba2c5078593036956494bd6ccb69e903
JungGeonwooYS <wjdrjsdn39@gmail.com>
2023-03-19 15:31:17 +0900

    add 2 people

---
+++ dsl.py
@@ -0,0 +1,11 @@
+class dsl(object):
+    def __init__(self, name):
+        self.name = name
+    def whois(self):
+        if self.name == "soeun":
+            print(f"{self.name} is vice president")
+        elif self.name == "geonwoo":
+            print(f"{self.name} is president")
+        else:
+            print(f"{self.name} 이 누구임?")
+
