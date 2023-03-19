14e69874ee318d70366f5190356873b86757cdf9
JungGeonwooYS <wjdrjsdn39@gmail.com>
2023-03-19 16:06:10 +0900

    add namhoon

--- dsl.py
+++ dsl.py
@@ -8,6 +8,7 @@ class dsl(object):
             print(f"{self.name} is president")
         elif self.name == "gyuwon":
             print(f"{self.name} is account")
+        elif self.name == "namhoon":
+            print(f"{self.name} is StudyKing")
         else:
             print(f"{self.name} 이 누구임?")
-
