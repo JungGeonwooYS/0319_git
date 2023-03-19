96e85088671b9d6ea3121b3d31846173ccf13e9b
JungGeonwooYS <wjdrjsdn39@gmail.com>
2023-03-19 15:39:53 +0900

    add gyuwon

--- dsl.py
+++ dsl.py
@@ -6,6 +6,8 @@ class dsl(object):
             print(f"{self.name} is vice president")
         elif self.name == "geonwoo":
             print(f"{self.name} is president")
+        elif self.name == "gyuwon":
+            print(f"{self.name} is account")
         else:
             print(f"{self.name} 이 누구임?")
 
