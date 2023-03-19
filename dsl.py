9c6d641ad29940ee4d331982ff6364f582ea0fa8
JungGeonwooYS <wjdrjsdn39@gmail.com>
2023-03-19 16:00:40 +0900

    add chanhyung

--- dsl.py
+++ dsl.py
@@ -8,5 +8,7 @@ class dsl(object):
             print(f"{self.name} is president")
         elif self.name == "chawon":
             print(f"{self.name} is HongBoKing")
+        elif self.name == "chanhyung":
+            print(f"{self.name} is StudyKing")
         else:
             print(f"{self.name} 이 누구임?")
